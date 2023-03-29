d7a9759f548e5c49cc48c1cfa9ad0de0d5c9fce3
Ghostryderen <129066941+Ghostryderen@users.noreply.github.com>
2023-03-29 07:57:55 +0000

    Add files via upload

--- Scripts.js
+++ Scripts.js
@@ -1,7 +1,7 @@
 // Get the button:
 let mybutton = document.getElementById('myBtn');
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
+window.onscroll = function (scrollTop) {
   scrollFunction()
 };
 function scrollFunction() {
@@ -17,20 +17,3 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
